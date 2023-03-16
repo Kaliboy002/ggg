905fb8b273e0b43bbc7b1cd51216edf5850efd57
Felix Rosberg <felixrosberg@users.noreply.huggingface.co>
2023-03-16 10:07:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio
-from huggingface_hub import Repository
+from huggingface_hub import Repository, duplicate_space
 import os
 
 from utils.utils import norm_crop, estimate_norm, inverse_estimate_norm, transform_landmark_points, get_lm
@@ -18,8 +18,8 @@ token = os.environ['model_fetch']
 
 opt = SwapOptions().parse()
 
-retina_repo = Repository(local_dir="retina_model", clone_from="felixrosberg/retinaface_resnet50",
-                         private=True, use_auth_token=token, git_user="felixrosberg")
+retina_repo = duplicate_space(to_id="retina_model", from_id="felixrosberg/retinaface_resnet50",
+                         private=True, token=token)
 
 from retina_model.models import *
 
