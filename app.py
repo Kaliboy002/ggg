9fb1282a5383ccf014a7b5837c834e2e4a372ed0
Felix Rosberg <felixrosberg@users.noreply.huggingface.co>
2023-07-07 18:59:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,65 @@ blend_mask_base[80:244, 32:224] = 1
 blend_mask_base = gaussian_filter(blend_mask_base, sigma=7)
 
 
+theme = gr.themes.Monochrome(
+    secondary_hue="emerald",
+    neutral_hue="teal",
+).set(
+    body_background_fill='*primary_950',
+    body_background_fill_dark='*secondary_950',
+    body_text_color='*primary_50',
+    body_text_color_dark='*secondary_100',
+    body_text_color_subdued='*primary_300',
+    body_text_color_subdued_dark='*primary_300',
+    background_fill_primary='*primary_600',
+    background_fill_primary_dark='*primary_400',
+    background_fill_secondary='*primary_950',
+    background_fill_secondary_dark='*primary_950',
+    border_color_accent='*secondary_600',
+    border_color_primary='*secondary_50',
+    border_color_primary_dark='*secondary_50',
+    color_accent='*secondary_50',
+    color_accent_soft='*primary_500',
+    color_accent_soft_dark='*primary_500',
+    link_text_color='*secondary_950',
+    link_text_color_dark='*primary_50',
+    link_text_color_active='*primary_50',
+    link_text_color_active_dark='*primary_50',
+    link_text_color_hover='*primary_50',
+    link_text_color_hover_dark='*primary_50',
+    link_text_color_visited='*primary_50',
+    block_background_fill='*primary_950',
+    block_background_fill_dark='*primary_950',
+    block_border_color='*secondary_500',
+    block_border_color_dark='*secondary_500',
+    block_info_text_color='*primary_50',
+    block_info_text_color_dark='*primary_50',
+    block_label_background_fill='*primary_950',
+    block_label_background_fill_dark='*secondary_950',
+    block_label_border_color='*secondary_500',
+    block_label_border_color_dark='*secondary_500',
+    block_label_text_color='*secondary_500',
+    block_label_text_color_dark='*secondary_500',
+    block_title_background_fill='*primary_950',
+    panel_background_fill='*primary_950',
+    panel_border_color='*primary_950',
+    checkbox_background_color='*primary_950',
+    checkbox_background_color_dark='*primary_950',
+    checkbox_background_color_focus='*primary_950',
+    checkbox_border_color='*secondary_500',
+    input_background_fill='*primary_800',
+    input_background_fill_focus='*primary_950',
+    input_background_fill_hover='*secondary_950',
+    input_placeholder_color='*secondary_950',
+    slider_color='*primary_950',
+    slider_color_dark='*primary_950',
+    table_even_background_fill='*primary_800',
+    table_odd_background_fill='*primary_600',
+    button_primary_background_fill='*primary_800',
+    button_primary_background_fill_dark='*primary_800'
+)
+
+
 def run_inference(target, source, slider, adv_slider, settings):
     try:
         source = np.array(source)
@@ -193,7 +252,37 @@ article = """
 Demo is based of recent research from my Ph.D work. Results expects to be published in the coming months.
 """
 
-iface = gradio.Interface(run_inference,
+with gr.Blocks(theme=theme) as blk_demo:
+        gr.Markdown(value="# Face Dancer")
+        with gr.Row():
+            with gr.Column():
+                with gr.Box():
+                    trg_in = gr.Image(type="pil", label='Target').style(height=300)
+                    src_in = gr.Image(type="pil", label='Source').style(height=300)
+                with gr.Row():
+                        b1 = gr.Button("Face Swap")
+                with gr.Row():
+                        with gr.Accordion("Options", open=False):
+                            chk_in = gr.CheckboxGroup(["Compare",
+                                                       "Anonymize",
+                                                       "Reconstruction Attack",
+                                                       "Adversarial Defense"],
+                                                      label="Mode",
+                                                      info="Anonymize mode? "
+                                                           "Apply reconstruction attack? "
+                                                           "Apply defense against reconstruction attack?")
+                            def_in = gr.Slider(0, 100, value=100,
+                                               label='Anonymization ratio (%)')
+                            mrg_in = gr.Slider(0, 100, value=100,
+                                               label='Adversarial defense ratio (%)')
+            with gr.Column():
+                with gr.Box():
+                    ano_out = gr.Image(type="pil", label='Output').style(height=300)
+
+b1.click(run_inference, inputs=[trg_in, src_in, def_in, mrg_in, chk_in],
+                 outputs=[ano_out])
+
+"""iface = gradio.Interface(run_inference,
                          [gradio.Image(shape=None, type="pil", label='Target'),
                           gradio.Image(shape=None, type="pil", label='Source'),
                           gradio.Slider(0, 100, value=100, label="Anonymization ratio (%)"),
@@ -208,5 +297,5 @@ iface = gradio.Interface(run_inference,
                          description=description,
                          examples=examples,
                          article=article,
-                         layout="vertical")
-iface.launch()
+                         layout="vertical")"""
+blk_demo.launch()
